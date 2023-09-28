915f2dccb536aee79eaf8ce3607cb998bd8acb6d
Leozinsk <leozinsk@gmail.com>
2023-09-28 13:02:03 +0100

    feat: adding update item function, ref: some parts of the code

--- content-script.js
+++ content-script.js
@@ -1,13 +1,8 @@
 const input = document.getElementById('box-input')
+const form = document.getElementById('log')
 const addBtn = document.getElementById('add')
 const list = document.getElementById('list')
 let getChrome = []
-
-// 1 - receber data do chrome.storage
-// 2 - receber informação de input 
-// 3 - passar informação do input para HTML
-// 4 - passar informação do input para o chrome.storage
-// 5 - botão de remover, removendo item da lista chrome.storage e remover item da lista HTML
 
 chrome.storage.local.get({ 'todo': [] }, data => {
     if (data.todo) {
@@ -20,28 +15,20 @@ chrome.storage.local.get({ 'todo': [] }, data => {
     }
 })
 
-input.addEventListener('keypress', (e) => {
-    if (e.key === "Enter") {
-        e.preventDefault();
-        settingInfoFromInput(getChrome, input)
-        setItem(input.value)
-        input.value = ''
-    }
-})
-addBtn.addEventListener('click', (e) => {
-    settingInfoFromInput(getChrome, input)
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    settingInfoFromInput(getChrome)
     setItem(input.value)
     input.value = ''
 })
 
-function settingInfoFromInput(items, input) {
+function settingInfoFromInput(items) {
     const inputValue = input.value
     inputValue === "" ? void (0) : items.push(inputValue)
     chrome.storage.local.set({ todo: items }, () => {
         console.log("Value is set")
     })
 }
-
 
 function setItem(element) {
     if (element instanceof Array) {
@@ -53,32 +40,58 @@ function setItem(element) {
     } else {
         console.error('Unable to load')
     }
-    // const messageBtn = document.querySelectorAll('#message')
-
-
-    // messageBtn.forEach(el =>
-    //     console.log(el))
 }
 function appendList(element) {
     list.innerHTML += `
-            <li id="itemList" data-value="${element}">
-            <p id="message">${element !== undefined ? element : void (0)}</p>
-            <button type="button" id="removeBtn" class="material-symbols-outlined">close</button>
-            </li>
-            `
+    <li id="itemList" data-value="${element}">
+        <p id="message">${element !== undefined ? element : void (0)}</p>
+        <button type="button" id="removeBtn" class="material-symbols-outlined">close</button>
+    </li>`
     const removeBtn = document.querySelectorAll('#removeBtn')
     const itemsList = document.querySelectorAll('#itemList')
-    removeBtn.forEach(el => el.addEventListener('click', () => {
-        const valuedIndex = getChrome.indexOf(el.parentElement.dataset.value)
-        let spliced = getChrome.splice(valuedIndex, 1);
+    removeBtn.forEach((el, index) => el.addEventListener('click', () => {
+        let spliced = sliceItems(index)
         removeLi(spliced, itemsList, getChrome)
         settingInfoFromInput(getChrome, input)
-
     }))
+    itemsList.forEach((el, index) => el.addEventListener('click', changeTags(el, index)))
 }
+
+function changeTags(el, index) {
+    return (e) => {
+        if(e.target.tagName === 'P'){
+            let newElement = document.createElement('input')
+            newElement.setAttribute('type', 'text')
+            newElement.value = el.firstElementChild.textContent
+            newElement.className = 'inputList'
+            el.replaceChild(newElement, el.firstElementChild)
+            newElement.focus()
+            newElement.addEventListener('keydown', inputToParag(newElement))
+        }
+    }
+
+    function inputToParag(newElement) {
+        return (e) => {
+            if(e.keyCode === 13 || e.key === 'Enter'){
+                let backToPTag = document.createElement('p')
+                backToPTag.id = "message"
+                backToPTag.innerHTML = e.target.value
+                getChrome[index] = e.target.value
+                settingInfoFromInput(getChrome)
+                el.replaceChild(backToPTag, newElement)
+            }
+        }
+    }
+}
+
+function sliceItems(el) {
+    let spliced = getChrome.splice(el, 1)
+    return spliced
+}
+
 function removeLi(value, itemsList, Array) {
     itemsList.forEach((e, index) => e.value === value ? Array.splice(index, 1) : void (0))
     list.innerHTML = ''
     setItem(Array)
 }
-// chrome.storage.local.clear()x'
+
